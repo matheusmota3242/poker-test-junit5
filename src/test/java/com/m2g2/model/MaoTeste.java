998797c9d5c4da04a45fb18809813438e12118ce
matheusmota3242 <matheusmota3242@gmail.com>
2023-12-03 12:32:30 -0300

    versao com enum finalizada

--- src/test/java/com/m2g2/model/MaoTeste.java
+++ src/test/java/com/m2g2/model/MaoTeste.java
@@ -10,7 +10,7 @@ import com.m2g2.enums.ValorCarta;
 import com.m2g2.model.builder.CartaBuilder;
 import com.m2g2.model.builder.MaoBuilder;
 
-class MaoTeste {
+class MaoTest {
 
 	@ParameterizedTest
 	@CsvSource(value = {
